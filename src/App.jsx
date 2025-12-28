d51036ae17765a3956b04b3efcb2dc78debffe66
ismartashish <141064255+ismartashish@users.noreply.github.com>
2025-12-28 23:04:44 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -164,8 +164,8 @@ export default function App() {
               <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" />
               <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg" />
               <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg" />
-              <img src="https://cdn.jsdelivr.net/gh/simple-icons/simple-icons/icons/linkedin.svg" />
-              <img src="https://cdn.jsdelivr.net/gh/simple-icons/simple-icons/icons/openai.svg"/>
+              <img src="https://cdn.jsdelivr.net/gh/simple-icons/simple-icons/icons/linkedin.svg" style={{ filter: "invert(1)" }}/>
+              <img src="https://cdn.jsdelivr.net/gh/simple-icons/simple-icons/icons/openai.svg alt="ChatGPT" style={{ filter: "invert(1)" }}"/>
             </div>
           </div>
         </motion.div>
